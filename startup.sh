418915e3924c8ff457dcf28d1278e633140f9b28
native-git <mjs695@rutgers.edu>
2017-01-13 15:22:21 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 
 
 #echo "Yout must run this command as root...recommend typing sudo su"
-ip link set enxb827eb0636d1 down
-ip link set enx801f02b6eab0 down
-ip link set enx801f02b6eab0 up
-wpa_supplicant -Dwext -B -ienx801f02b6eab0 -c/etc/wpa_supplicant/wpa_supplicant.conf
-dhclient enx801f02b6eab0
+ip link set <CHANGE_ME> down
+ip link set wlan0 down
+ip link set wlan0 up
+wpa_supplicant -Dwext -B -i<CHANGE_ME> -c/etc/wpa_supplicant/wpa_supplicant.conf
+dhclient wlan0
 exit 0
 
